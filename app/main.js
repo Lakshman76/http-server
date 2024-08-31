3b0b81bd5327700ca2b95ac337db3c527ebaab51
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-31 11:01:21 +0530

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -4,10 +4,10 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+const server = net.createServer((socket) => {
+  socket.on("close", () => {
+    socket.end();
+  });
+});
+
+server.listen(4221, "localhost");

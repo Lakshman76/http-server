47829780f0c2425eddcf32c1add60508aa3b6730
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-31 12:31:28 +0530

    stage - respond with body

--- app/main.js
+++ app/main.js
@@ -3,11 +3,15 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
+
     const requestLine = request.split("\r\n")[0];
     const path = requestLine.split(" ")[1];
 
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    if (path.startsWith("/echo/")) {
+      const message = path.split("/")[2]; // --> /echo/abc
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
+      );
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }

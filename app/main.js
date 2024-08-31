54231af29c3eb457a1eca59abdf102099bbaa590
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-31 11:55:27 +0530

    stage - extract url path

--- app/main.js
+++ app/main.js
@@ -1,13 +1,12 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    if (request.startsWith("GET /")) {
+    const requestLine = request.split("\r\n")[0];
+    const path = requestLine.split(" ")[1];
+
+    if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");

468650556b2407373f8a7f9428baa252adb49c2e
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-31 11:22:09 +0530

    stage - respond with 200

--- app/main.js
+++ app/main.js
@@ -1,10 +1,11 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
   });

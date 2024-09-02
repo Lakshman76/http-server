651ec626b8b7b2a82ab6be57d83456d68ceb8f86
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-09-02 10:41:57 +0530

    read request body

--- app/main.js
+++ app/main.js
@@ -2,12 +2,14 @@ const net = require("net");
 const path = require("path");
 const fs = require("fs");
 
-
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
 
     const requestLine = request.split("\r\n")[0];
+    const requestBody = request.split("\r\n")[7]; // data
+
+    const httpMethod = requestLine.split(" ")[0];
     const urlPath = requestLine.split(" ")[1];
 
     if (urlPath === "/") {
@@ -25,8 +27,8 @@ const server = net.createServer((socket) => {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentValue.length}\r\n\r\n${userAgentValue}`
       );
-    } else if (urlPath.startsWith("/files/")) {
-        const directory = process.argv[3];
+    } else if (httpMethod === "GET" && urlPath.startsWith("/files/")) {
+      const directory = process.argv[3];
       const filename = urlPath.split("/")[2]; // --> /files/filename
       const filePath = path.join(directory, filename);
 
@@ -36,9 +38,18 @@ const server = net.createServer((socket) => {
           `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}\r\n`
         );
         socket.write(fileContent);
-    } else {
-        
+      } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    } else if (httpMethod === "POST" && urlPath.startsWith("/files/")) {
+      const directory = process.argv[3];
+      const filename = urlPath.split("/")[2]; // --> /files/filename
+      const filePath = path.join(directory, filename);
+      try {
+        fs.writeFileSync(filePath, requestBody); // create and write content
+        socket.write("HTTP/1.1 201 Created\r\n\r\n");
+      } catch (err) {
+        socket.write("Error writing file:", err);
       }
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
